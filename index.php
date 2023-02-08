9277ac87c51261fa927857f43df9cbc7b7ec008c
Kovpak81 <106636803+Kovpak81@users.noreply.github.com>
2023-02-08 20:33:35 +1000

    Update index.php

--- index.php
+++ index.php
@@ -1,5 +1,6 @@
 <?php
 
-ini_set('display_errors', 1);
+// вывод ошибок отключен, т.к. сейчас он не нужен и при первом вхождении, когда переменная  $routes = $_GET['url']; будет не определена, т.к. еще не будет значения url, будет выводтся ошибка 
+ini_set('display_errors', 0);
 require_once 'application/bootstrap.php';
 // $url = $_GET['url'];echo $url;
